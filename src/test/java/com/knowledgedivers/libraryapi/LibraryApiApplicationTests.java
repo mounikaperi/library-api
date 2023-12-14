b605d4a5cd45681fce82f4ac0482330c9766c18a
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-12-14 18:47:49 +0530

    Initial commit

---
+++ src/test/java/com/knowledgedivers/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowledgedivers.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
