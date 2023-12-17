187f9abbed374d36bf282598db4e95faab196292
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-12-17 19:59:23 +0530

    Started Working on Reviews

--- src/main/java/com/knowledgedivers/libraryapi/config/MyDataRestConfig.java
+++ src/main/java/com/knowledgedivers/libraryapi/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.knowledgedivers.libraryapi.config;
 
 import com.knowledgedivers.libraryapi.entity.Book;
+import com.knowledgedivers.libraryapi.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -19,7 +20,11 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 HttpMethod.PUT,
         };
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
+
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
+
         cors.addMapping(config.getBasePath() + "/**")
                 .allowedOrigins(theAllowedOrigins).allowedHeaders("*");
 
---
+++ src/main/java/com/knowledgedivers/libraryapi/dao/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.knowledgedivers.libraryapi.dao;
+
+import com.knowledgedivers.libraryapi.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Page<Review> findBookById(@RequestParam("book_id") Long bookId, Pageable pageable);
+}
