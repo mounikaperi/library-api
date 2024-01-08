60b268dce44fd4bac1fda91ec4085140a2ca8984
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-01-08 21:38:52 +0530

    Returning current loans of a user

--- src/main/java/com/knowledgedivers/libraryapi/controller/BookController.java
+++ src/main/java/com/knowledgedivers/libraryapi/controller/BookController.java
@@ -3,10 +3,13 @@ package com.knowledgedivers.libraryapi.controller;
 import com.knowledgedivers.libraryapi.dao.CheckoutRepository;
 import com.knowledgedivers.libraryapi.entity.Book;
 import com.knowledgedivers.libraryapi.entity.Checkout;
+import com.knowledgedivers.libraryapi.responsemodels.ShelfCurrentLoansResponse;
 import com.knowledgedivers.libraryapi.service.BookService;
 import com.knowledgedivers.libraryapi.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -33,5 +36,10 @@ public class BookController {
     public Book checkoutBook(@RequestHeader(value="Authorization") String token, @RequestParam Long bookId) throws Exception {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
+    }
+    @GetMapping("/secure/currentloans")
+    public List<ShelfCurrentLoansResponse> currentLoans(@RequestHeader(value="Authorization") String token) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return bookService.currentLoans(userEmail);
     }
 }
--- src/main/java/com/knowledgedivers/libraryapi/service/BookService.java
+++ src/main/java/com/knowledgedivers/libraryapi/service/BookService.java
@@ -10,10 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
@@ -59,5 +59,17 @@ public class BookService {
             bookIdList.add(i.getBookId());
         }
         List<Book> books = bookRepository.findBooksByBookIds(bookIdList);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        for (Book book: books) {
+            Optional<Checkout> checkout = checkoutList.stream().filter(currentBook -> Objects.equals(currentBook.getBookId(), book.getId())).findFirst();
+            if (checkout.isPresent()) {
+                Date d1 = sdf.parse(checkout.get().getReturnDate());
+                Date d2 = sdf.parse(LocalDate.now().toString());
+                TimeUnit time = TimeUnit.DAYS;
+                long difference_In_Time = time.convert(d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
+                shelfCurrentLoansResponse.add(new ShelfCurrentLoansResponse(book, (int) difference_In_Time));
+            }
+        }
+        return shelfCurrentLoansResponse;
     }
 }
