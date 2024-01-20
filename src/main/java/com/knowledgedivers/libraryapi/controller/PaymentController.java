c5847cc7bbd975c5aaf462aa2ef22ac5fae6c73f
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-01-20 18:46:21 +0530

    Working on Payment

--- src/main/java/com/knowledgedivers/libraryapi/controller/PaymentController.java
+++ src/main/java/com/knowledgedivers/libraryapi/controller/PaymentController.java
@@ -1,11 +1,13 @@
 package com.knowledgedivers.libraryapi.controller;
 
+import com.knowledgedivers.libraryapi.requestmodels.PaymentInfoRequest;
 import com.knowledgedivers.libraryapi.service.PaymentService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("https://localhost:3000")
 @RestController
@@ -13,10 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
     private PaymentService paymentService;
 
+    private PaymentInfoRequest paymentInfoRequest;
+
     @Autowired
-    public PaymentController(PaymentService paymentService) {
+    public PaymentController(PaymentService paymentService, PaymentInfoRequest paymentInfoRequest) {
         this.paymentService = paymentService;
+        this.paymentInfoRequest = paymentInfoRequest;
     }
 
     @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfoRequest paymentInfoRequest) throws Exception {
+        PaymentIntent paymentIntent = paymentService.createPaymentIntent(paymentInfoRequest);
+        String paymentResponse = paymentIntent.toJson();
+        return new ResponseEntity<>(paymentResponse, HttpStatus.OK);
+    }
+    
 }
